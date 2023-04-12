ecdf43e55fc62567ec14a9118770b02d8c0636b2
Alex Maszanski <39808040+Wreemde@users.noreply.github.com>
2023-04-12 20:01:11 +0400

    Created encryption.py file

---
+++ passprotector/encryption.py
@@ -0,0 +1,12 @@
+# TODO: import libraries
+class PasswordManager:
+    def __init__():
+    # TODO: add more functionality
+    
+    def encrypt():
+        return
+    # TODO: add more functionality
+    
+    def decrypt():
+        return
+    # TODO: add more functionality

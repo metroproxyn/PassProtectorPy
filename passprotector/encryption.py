53b2c5f2eb20427e985f20d5940ce228f2a0e592
Alex Maszanski <39808040+Wreemde@users.noreply.github.com>
2023-04-13 15:23:46 +0400

    The variables in encryption.py have been set

--- passprotector/encryption.py
+++ passprotector/encryption.py
@@ -3,12 +3,16 @@ from cryptography.fernet import Fernet
 
 class PasswordManager:
     def __init__(self, key):
+        self.key =
+        self.fernet = 
     # TODO: add more functionality
     
     def encrypt(self, password):
-        return
+        encrypted_password = 
+        return 
     # TODO: add more functionality
     
     def decrypt(self, encrypted_password):
+        decrypted_password = 
         return
     # TODO: add more functionality

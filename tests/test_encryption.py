fd30b1c6da3ad48a4f80b8c709df2fec7871097e
Alex Maszanski <39808040+Wreemde@users.noreply.github.com>
2023-04-14 10:04:08 +0400

    Skeleton functions and class for the encryption test

--- tests/test_encryption.py
+++ tests/test_encryption.py
@@ -1,1 +1,10 @@
+import unittest
+from passprotector.encryption import encrypt, decrypt
+
 #TODO: write the test for encryption.py module
+class EncryptionTestCase(unittest.TestCase):
+    def test_encrypt_decrypt():
+        #TODO: add more functionality
+
+    def test_invalid_inputs():
+        #TODO: add more functionality

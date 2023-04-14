db634bbb0f354d037d35ccf0bec21ff89c2e9675
Alex Maszanski <39808040+Wreemde@users.noreply.github.com>
2023-04-14 10:34:55 +0400

    test_encrypt_decrypt function has been written

--- tests/test_encryption.py
+++ tests/test_encryption.py
@@ -2,9 +2,24 @@ import unittest
 from passprotector.encryption import encrypt, decrypt
 
 #TODO: write the test for encryption.py module
-class EncryptionTestCase(unittest.TestCase):
-    def test_encrypt_decrypt():
+class EncryptionTestCase(unittest.TestCase): # class that defines a collection of individual test methods.
+    def test_encrypt_decrypt(self):
         #TODO: add more functionality
+        plaintext = b"let's test our cipher" # In Python, a byte string is denoted
+        key= b"secretkey"                    # by adding the character 'b' before the start of the string. 
+        ciphertext = encrypt(plaintext, key)
+        decrypted_plaintext = decrypt(ciphertext, key)
+        self.assertEqual(plaintext, decrypted_plaintext)
+        """
+        self.assertEqual() is an assertion method in the unittest library 
+        that verifies that the actual result of a test matches the expected result. 
+        In the line above, this method is used to compare the plaintext variable 
+        (which is the original message) to the decrypted_plaintext variable 
+        (which is the decrypted message). If they match, the test passes. 
+        If they don't match, the test fails and an error message is displayed. 
+        This is a common way to verify the correctness of a function's output.
+        """
 
-    def test_invalid_inputs():
-        #TODO: add more functionality
+
+    """def test_invalid_inputs():
+        #TODO: add more functionality"""

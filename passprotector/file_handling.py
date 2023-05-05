f555ef6dd7dada0816e4d44cea900f64517e497e
Alex Maszanski <39808040+Wreemde@users.noreply.github.com>
2023-05-05 19:24:28 +0400

    Skeleton content for the file_handling module

--- passprotector/file_handling.py
+++ passprotector/file_handling.py
@@ -1,1 +1,22 @@
 #TODO: add requirement functional
+import os
+
+def read_file():
+    "'""Read data from a file and return it as a string."""
+    return
+
+def write_file():
+    """Write data to a file given a file path and data to write."""
+    return
+
+def file_exists():
+    """Check if the file exists."""
+    return 
+
+def create_file():
+    """Create e new file given a file path."""
+    return
+
+def delete_file():
+    """Delete a file given a file path."""
+    return

e646b9c2983f876bb52478d5cee1d98a8d9bb51a
Alex Maszanski <39808040+Wreemde@users.noreply.github.com>
2023-05-08 14:17:58 +0400

    Skeleton content for the test_file_handling module

---
+++ tests/test_file_handling.py
@@ -0,0 +1,23 @@
+import os
+import unittest
+from src.file_handling import read_data
+
+class TestFileHandling(unittest.TestCase):
+
+    def setup(self):
+        #TODO: add more functionality
+    
+    def tearDown(self):
+        #TODO: add more functionality
+    
+    def test_write_to_file(self):
+        #TODO: add more functionality
+    
+    def test_read_to_file(self):
+        #TODO: add more functionality
+    
+    def test_file_exists(self):
+        #TODO: add more functionality
+
+if __name__ == '__main__':
+    unittest.main()
